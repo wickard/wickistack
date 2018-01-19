20aab64a5e8a3bdf169cd95b126de2f3b3781541
Josh Carlson <carlson.joshuaph@gmail.com>
2018-01-19 17:06:53 -0600

    first commit

---
+++ models/index.js
@@ -0,0 +1,51 @@
+var Sequelize = require('sequelize');
+var db = new Sequelize('postgres://localhost:5432/wikistack', {
+  logging: false
+});
+
+const Page = db.define('page', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
+    },
+  urlTitle: {
+    type: Sequelize.STRING,
+    isUrl: true,
+    allowNull: false
+  },
+  content: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  status: {
+    type: Sequelize.ENUM('open', 'closed'),
+    allowNull: false
+  },
+    date: {
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.NOW
+  },
+},
+  {
+  getterMethods: {
+      route() {
+        return `/wiki/${this.urlTitle}`
+      }
+    }
+  })
+
+const User = db.define('user', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    isEmail: true,
+    allowNull: false
+  }
+})
+
+module.exports = {
+  Page, User, db
+}
